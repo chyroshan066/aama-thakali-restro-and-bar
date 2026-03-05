64df4dd13245ef7ac8ac34bbf343112bbf6a2d0a
chyroshan066 <chyroshan066@gmail.com>
2026-03-05 15:42:40 +0545

    feat(backend): changed environment variables in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,7 @@
-const http = require("http");
 const app = require("./app");
 const { pool } = require("./config/db");
 
-const PORT = process.env.PORT || 5000;
-
 let isConnected = false;
-
-// async function startServer() {
-//   try {
-//     // Verify database connection on startup for easier debugging
-//     await pool.query('SELECT 1');
-//     const server = http.createServer(app);
-
-//     server.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//       console.log('Database connection successful');
-//     });
-//   } catch (err) {
-//     console.error('Failed to start server due to database connection error:', err.message);
-//     process.exit(1);
-//   }
-// }
 
 async function connectToPostgres() {
   try {
@@ -45,8 +26,6 @@ app.use(async (req, res, next) => {
   }
   next();
 });
-
-// startServer();
 
 //don't use app.listen() in vercel.json
 module.exports = app;
