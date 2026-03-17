ae3ea8ee061c334ea191cbc5819c6f8be7909086
unknown <chyroshan066@gmail.com>
2026-03-17 23:38:26 +0545

    changed color and SEO added

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,57 +1,16 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactStrictMode: true,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com"
-//       }
-//     ]
-//   }
-// };
-
-// export default nextConfig;
-
-
-
-
-
-
-
-
-
-
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: "/dashboard",
-        destination: `${process.env.NEXT_PUBLIC_DASHBOARD_URL}/dashboard`,
-      },
-      {
-        source: "/dashboard/:path*",
-        destination: `${process.env.NEXT_PUBLIC_DASHBOARD_URL}/dashboard/:path*`,
-      },
-    ];
-  },
+        hostname: "res.cloudinary.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
